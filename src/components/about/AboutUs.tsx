468f413a0ca246cf0c42dd7a45306c0a6771b1ed
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-09-17 22:30:03 +0530

    layout of about page changed

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -5,21 +5,25 @@ const AboutUs = () => {
     return (
         <section className="about-us">
             <div className="about-cards">
-                <img src="/energy1.png" alt="team" />
-                <div className="about-cards-inner">
+                <div className="about-cards-inner-top">
+                    <img src="/energy1.png" alt="team" />
+                </div>
+                <div className="about-cards-inner-bottom">
                     <h1>Our Story</h1>
                     <h4>Our journey began with a simple yet powerful idea: that energy could be smarter, cleaner and more responsible. Founded by a seasoned team of oil & gas industry veterans, Arkah is a next-generation energy company born from a shared vision to redefine the future of energy. With deep industry expertise, strategic foresight and an innovation-first mindset, we are
-                        committed to building a sustainable, reliable, and resilient energy future.
+                        committed to building a sustainable, reliable, and resilient energy future.</h4>
 
-                        We believe that the energy industry must evolve to meet the demands of a rapidly changing world. To remain relevant and responsible, it must embrace innovation, sustainability and adaptability. At Arkah, we are fueling this evolution - harnessing cutting edge technologies and cost-effective solutions with sustainable practices that serve both people and the planet. From advancing renewable energy and digital innovation to unlocking untapped fossil fuel reserves with integrity, we are redefining what energy means for the modern world.
+                    <h4>We believe that the energy industry must evolve to meet the demands of a rapidly changing world. To remain relevant and responsible, it must embrace innovation, sustainability and adaptability. At Arkah, we are fueling this evolution - harnessing cutting edge technologies and cost-effective solutions with sustainable practices that serve both people and the planet. From advancing renewable energy and digital innovation to unlocking untapped fossil fuel reserves with integrity, we are redefining what energy means for the modern world.</h4>
 
-                        Our story is still unfolding, but our mission is clear - Powering progress with purpose & fueling future responsibly.
+                    <h4>Our story is still unfolding, but our mission is clear - Powering progress with purpose & fueling future responsibly.
                     </h4>
                 </div>
             </div>
             <div className="about-cards">
-                <img src="/ESG-2.jpg" alt="team" />
-                <div className="about-cards-inner">
+                <div className="about-cards-inner-top">
+                    <img src="/ESG2.jpg" alt="team" />
+                </div>
+                <div className="about-cards-inner-bottom">
                     <h1>Our Purpose</h1>
                     <h4>At Arkah, our mission is to revolutionize the global energy landscape through responsible innovation, reliable and efficient energy solutions and an unwavering pursuit of operational
                         excellence. Driven by integrity and a long-term vision, we are committed to building a
@@ -28,8 +32,10 @@ const AboutUs = () => {
                 </div>
             </div>
             <div className="about-cards">
-                <img src="/Team.jpg" alt="team" />
-                <div className="about-cards-inner">
+                <div className="about-cards-inner-top">
+                    <img src="/Team.jpg" alt="team" />
+                </div>
+                <div className="about-cards-inner-bottom">
                     <h1>Our Team</h1>
                     <h4>Driven by knowledge, inspired by sustainability and united by purpose — Team Arkah shapes energy that empowers today and protects tomorrow.
                     </h4>
