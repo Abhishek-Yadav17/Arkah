49ae65eff1f6b53f398b8c712bfa80f8f67182c2
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-10-06 18:53:45 +0530

    video modified

--- src/components/banner/BannerV3.tsx
+++ src/components/banner/BannerV3.tsx
@@ -4,7 +4,7 @@ import '../../assets/styles/BannerV3.scss';
 const BannerV3: React.FC = () => {
     return (
         <div className="about-hero-section">
-            <video src="/Arkah1.mp4" autoPlay loop muted></video>
+            <video src="/arkah.mp4" autoPlay loop muted></video>
         </div>
     );
 };
