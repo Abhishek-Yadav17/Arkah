af99cab5463405744228058b15ea38d5865efb7e
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-11-01 13:29:14 +0530

    form is working now

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -1,65 +1,70 @@
-
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 import { toast } from "react-toastify";
 
-interface FormEventHandler {
-    (event: React.FormEvent<HTMLFormElement>): void;
-}
-
 const ContactForm = () => {
+    const formRef = useRef<HTMLFormElement>(null);
 
-    const handleForm: FormEventHandler = (event) => {
-        event.preventDefault()
-        const form = event.target as HTMLFormElement;
-        form.reset()
-        toast.success("Thanks For Your Email!")
-    }
+    const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!formRef.current) return;
+
+        emailjs
+            .sendForm(
+                "service_d2f5eh3",
+                "template_jmmmc7i",
+                formRef.current,
+                "9fnsmCKxRyLmUuAFg"
+            )
+            .then(
+                () => {
+                    toast.success("Thanks for your email!");
+                    formRef.current?.reset();
+                },
+                (error) => {
+                    toast.error("Failed to send. Please try again.");
+                    console.error("EmailJS error:", error);
+                }
+            );
+    };
 
     return (
-        <>
-            <form className="contact-form contact-form" onSubmit={handleForm}>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="form-group">
-                            <input className="form-control" id="name" name="name" placeholder="Name" type="text" required autoComplete="off" />
-                            <span className="alert-error" />
-                        </div>
+        <form ref={formRef} className="contact-form" onSubmit={handleForm}>
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="form-group">
+                        <input className="form-control" id="name" name="name" placeholder="Name" type="text" required />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-6">
-                        <div className="form-group">
-                            <input className="form-control" id="email" name="email" placeholder="Email*" type="email" required autoComplete="off" />
-                            <span className="alert-error" />
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="form-group">
-                            <input className="form-control no-arrows" id="phone" name="phone" placeholder="Phone" type="number" required autoComplete="off" />
-                            <span className="alert-error" />
-                        </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="form-group">
+                        <input className="form-control" id="email" name="email" placeholder="Email*" type="email" required />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="form-group comments">
-                            <textarea className="form-control" id="comments" name="comments" placeholder="Tell Us About Project *" required autoComplete="off" />
-                        </div>
+                <div className="col-lg-6">
+                    <div className="form-group">
+                        <input className="form-control no-arrows" id="phone" name="phone" placeholder="Phone" type="number" required />
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <button type="submit" name="submit" id="submit">
-                            <i className="ri-send-plane-line" /> Get in Touch
-                        </button>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="form-group comments">
+                        <textarea className="form-control" id="comments" name="comments" placeholder="Tell Us About Project *" required />
                     </div>
                 </div>
-
-                {/* Alert Message */}
-                <div className="col-lg-12 alert-notification">
-                    <div id="message" className="alert-msg" />
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <button type="submit" id="submit">
+                        <i className="ri-send-plane-line" /> Get in Touch
+                    </button>
                 </div>
-            </form>
-        </>
+            </div>
+        </form>
     );
 };
 
