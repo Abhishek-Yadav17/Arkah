08c59dce311b50b12b2c633566de6143979dbd71
Abhishek-Yadav17 <17abhishekadav@gmail.com>
2025-11-04 15:23:54 +0530

    made navbar responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
 import '../../assets/styles/HeaderV2.scss';
 
-gsap.registerPlugin(ScrollTrigger);
-
 const HeaderV2: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setMenuOpen(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
     return (
         <header className="header-v2">
             <div className="container">
                 <Link to="/"><img src="/logo.png" alt="logo" /></Link>
-                <nav className="nav">
+
+                <nav className="nav desktop-nav">
                     <ul>
                         <li className="dropdown">
-                            <Link to="/about2">
-                                Who We Are
-                            </Link>
+                            <Link to="/about2">Who We Are</Link>
                             <ul className="dropdown-menu">
                                 <li><Link to="/about2#our-story">Our Story</Link></li>
                                 <li><Link to="/about2#our-purpose">Our Purpose</Link></li>
@@ -26,9 +32,7 @@ const HeaderV2: React.FC = () => {
                         </li>
 
                         <li className="dropdown">
-                            <Link to="/what-we-do">
-                                What We Do
-                            </Link>
+                            <Link to="/what-we-do">What We Do</Link>
                             <ul className="dropdown-menu">
                                 <li><Link to="/what-we-do#operating-philosophy">Operating Philosophy</Link></li>
                                 <li><Link to="/what-we-do#exploration-production">Responsible Hydrocarbons</Link></li>
@@ -41,6 +45,40 @@ const HeaderV2: React.FC = () => {
                         <li><Link to="/newsroom">News Room</Link></li>
                         <li><Link to="/careers">Careers</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+
+                {/* Mobile menu icon */}
+                <div className="mobile-menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </div>
+
+                {/* Mobile menu */}
+                <nav className={`nav mobile-nav ${menuOpen ? 'open' : ''}`}>
+                    <ul>
+                        <li>
+                            <Link to="/about2" onClick={() => setMenuOpen(false)}>Who We Are</Link>
+                            <ul>
+                                <li><Link to="/about2#our-story" onClick={() => setMenuOpen(false)}>Our Story</Link></li>
+                                <li><Link to="/about2#our-purpose" onClick={() => setMenuOpen(false)}>Our Purpose</Link></li>
+                                <li><Link to="/about2#our-team" onClick={() => setMenuOpen(false)}>Our Team</Link></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <Link to="/what-we-do" onClick={() => setMenuOpen(false)}>What We Do</Link>
+                            <ul>
+                                <li><Link to="/what-we-do#operating-philosophy" onClick={() => setMenuOpen(false)}>Operating Philosophy</Link></li>
+                                <li><Link to="/what-we-do#exploration-production" onClick={() => setMenuOpen(false)}>Responsible Hydrocarbons</Link></li>
+                                <li><Link to="/what-we-do#renewable-natural-gas" onClick={() => setMenuOpen(false)}>Renewable Natural Gas</Link></li>
+                            </ul>
+                        </li>
+
+                        <li><Link to="/sustainability" onClick={() => setMenuOpen(false)}>Sustainability</Link></li>
+                        <li><Link to="/investors" onClick={() => setMenuOpen(false)}>Investors</Link></li>
+                        <li><Link to="/newsroom" onClick={() => setMenuOpen(false)}>News Room</Link></li>
+                        <li><Link to="/careers" onClick={() => setMenuOpen(false)}>Careers</Link></li>
+                        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
                     </ul>
                 </nav>
             </div>
